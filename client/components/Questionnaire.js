eaa0b3b77d591307db5666fdc880094ba310f6fb
Brailor <ygatravat@gmail.com>
2018-01-07 21:48:12 +0100

    kerdoiv nemsokara kesz

--- client/components/Questionnaire.js
+++ client/components/Questionnaire.js
@@ -1,31 +1,84 @@
 import React, { Component } from 'react';
 
 class Questionnaire extends Component {
-  constructor() {
-    super();
-    this.state = {
-      questionnaire: null
-    };
-  }
-  async componentDidMount() {
-    const { id } = this.props.match.params;
-    const res = await fetch(`/kerdoiv/${id}`);
-    const data = await res.json();
-    console.log(data);
-
-    this.setState({ questionnaire: data[0] });
-  }
-  render() {
-    const { questionnaire } = this.state;
-    if (questionnaire === null || questionnaire === undefined) {
-      return (
-        <div>
-          <h2>Loading....</h2>
-        </div>
-      );
+    constructor() {
+        super();
+        this.state = {
+            questionnaire: null
+        };
     }
-    return <div className="card"> {questionnaire.title}</div>;
-  }
+    async componentDidMount() {
+        const { id } = this.props.match.params;
+        const res = await fetch(`/kerdoiv/${id}`);
+        const data = await res.json();
+        console.log(data);
+
+        this.setState({ questionnaire: data });
+    }
+    render() {
+        const { questionnaire } = this.state;
+        if (questionnaire === null || questionnaire === undefined) {
+            return (
+                <div>
+                    <h2>Loading....</h2>
+                </div>
+            );
+        }
+        return (
+            <div className="subjects-landing-page">
+                <div className="row">
+                    <div className="col-sm-8 offset-sm-2">
+                        <div className="card">
+                            <div className="card-header text-center">
+                                {questionnaire.title}
+                            </div>
+                            <div className="card-block">
+                                <h4>Kérdőív leírása:</h4>
+                                <p>{questionnaire.description}</p>
+                                {questionnaire.questions.map(
+                                    (question, ind) => (
+                                        <div
+                                            key={question.id}
+                                            className="question-block"
+                                        >
+                                            <div className=" text-center">
+                                                {`${ind + 1}. kérdés: `}
+                                                <b>{question.title}</b>
+                                            </div>
+                                            <br />
+                                            <div className="container">
+                                                {question.answerOpts.map(
+                                                    (answer, ind) => (
+                                                        <div
+                                                            key={ind}
+                                                            className="question"
+                                                        >
+                                                            <input
+                                                                type="checkbox"
+                                                                value={
+                                                                    answer.value
+                                                                }
+                                                            />
+                                                            {answer.value}
+                                                        </div>
+                                                    )
+                                                )}
+                                            </div>
+                                        </div>
+                                    )
+                                )}
+                            </div>
+                            <div className="card-footer text-center">
+                                <button className="btn btn-success btn-block">
+                                    Elküldés
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Questionnaire;
--- index.js
+++ index.js
@@ -6,42 +6,48 @@ const mockData = require('./mock/index.js');
 
 //react hot reload
 if (process.env.NODE_ENV != 'production') {
-  const config = require('./webpack.config.dev.js');
-  const webpack = require('webpack');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
+    const config = require('./webpack.config.dev.js');
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
 
-  const compiler = webpack(config);
-  app.use(
-    webpackDevMiddleware(compiler, {
-      noInfo: true,
-      publicPath: config.output.publicPath
-    })
-  );
-  app.use(webpackHotMiddleware(compiler));
+    const compiler = webpack(config);
+    app.use(
+        webpackDevMiddleware(compiler, {
+            noInfo: true,
+            publicPath: config.output.publicPath
+        })
+    );
+    app.use(webpackHotMiddleware(compiler));
 }
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.post('/login', (req, res) => {
-  const { username = '', password = '' } = req.body;
-  res.json({
-    success: true
-  });
+    const { username = '', password = '' } = req.body;
+    res.json({
+        success: true
+    });
+});
+
+//todo: szétszedni a router logikát
+
+app.get('/create/kerdoiv-lista', (req, res) => {
+    res.json(mockData);
 });
 
 app.get('/kerdoiv/:id', (req, res) => {
-  const { id } = req.params;
-  console.log(`Ez a kérdőív kell: ${id}`);
-  res.json(mockData);
+    const { id } = req.params;
+    console.log(`Ez a kérdőív kell: ${id}`);
+    res.json(mockData[0]);
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public', 'index.html'));
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
-app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Listening on :' + (process.env.PORT || 3000));
+    console.log('Listening on :' + (process.env.PORT || 3000));
 });
